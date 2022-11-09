f5c1323400088d70fa7e925f78e87b406ae6959c
joko-takafumi <jktkfm5535@gmail.com>
2022-11-10 00:57:33 +0900

    3-2

---
+++ 3/post_delete_done.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+
+<?php
+
+$id = $_POST['id'];
+
+$id = htmlspecialchars($id, ENT_QUOTES, 'UTF-8');
+
+try
+{
+	$dsn = 'mysql:dbname=twitter;host=localhost';
+	$user = 'root';
+	$password = '';
+	$dbh = new PDO($dsn, $user, $password);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	$sql = 'DELETE FROM post WHERE id=?';
+	$stmt = $dbh->prepare($sql);
+	$data[] = $id;
+	$stmt->execute($data);
+	
+	$dbh = null;
+}
+catch(Exception $e)
+{
+	print 'ただいま障害が発生しております。ご迷惑をお掛けしております。';
+	exit();
+}
+
+?>
+
+<h2>投稿の削除が完了しました。</h2>
+<form action="index.php">
+	<input type="button" value="投稿一覧へ戻る" onclick="history.back()">
+</form>
+</body>
+</html>
